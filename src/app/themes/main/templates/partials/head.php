9485b77bc49653bca97ac814fc4411101cb01b12
saraojelind <sara.ojelind@frojd.se>
2017-05-04 16:02:44 +0200

    Added favicons template

--- src/app/themes/main/templates/partials/head.php
+++ src/app/themes/main/templates/partials/head.php
@@ -2,5 +2,6 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php get_template_part('partials/favicons'); ?>
     <?php wp_head(); ?>
 </head>
