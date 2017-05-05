7d2d6914d698ca1d79923dd6b4cd16e39c9033aa
saraojelind <sara.ojelind@frojd.se>
2017-05-05 14:25:50 +0200

    Added an early return in cookie popup

--- src/app/themes/main/templates/partials/cookie-popup.php
+++ src/app/themes/main/templates/partials/cookie-popup.php
@@ -1,9 +1,10 @@
 <?php
-$cookiePopupText = get_field('cookie_popup_text', 'option');
+    $cookiePopupText = get_field('cookie_popup_text', 'option');
+    if(!$cookiePopupText)
+        return;
 ?>
-<?php if ($cookiePopupText) : ?>
-    <div class="cookie-popup">
-        <div class="cookie-popup__message"><?= $cookiePopupText ?></div>
-        <button class="cookie-popup__close-btn"><?=__('Close', 'sage') ?></button>
-    </div>
-<?php endif; ?>
+
+<div class="cookie-popup">
+    <div class="cookie-popup__message"><?= $cookiePopupText ?></div>
+    <button class="cookie-popup__close-btn"><?=__('Close', 'sage') ?></button>
+</div>
