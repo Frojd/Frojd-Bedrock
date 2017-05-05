95bad1123ce39bf56b609c9158ecaca0fcdce41a
saraojelind <sara.ojelind@frojd.se>
2017-05-05 08:30:23 +0200

    Removed primary sidebar and changed translations

--- src/app/themes/main/src/setup.php
+++ src/app/themes/main/src/setup.php
@@ -92,10 +92,7 @@ add_action('widgets_init', function () {
         'before_title' => '<h3>',
         'after_title' => '</h3>'
     ];
-    register_sidebar([
-            'name' => __('Primary', 'sage'),
-            'id' => 'sidebar-primary'
-        ] + $config);
+    
     register_sidebar([
             'name' => __('Footer', 'sage'),
             'id' => 'sidebar-footer'
