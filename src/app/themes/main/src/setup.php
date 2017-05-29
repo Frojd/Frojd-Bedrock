b0b7547c9e40ec1742c20dc2edfa0900542b41da
saraojelind <sara.ojelind@frojd.se>
2017-05-29 16:26:22 +0200

    Removed footer from widgets

--- src/app/themes/main/src/setup.php
+++ src/app/themes/main/src/setup.php
@@ -83,23 +83,6 @@ add_action('after_setup_theme', function () {
 });
 
 /**
- * Register sidebars
- */
-add_action('widgets_init', function () {
-    $config = [
-        'before_widget' => '<section class="widget %1$s %2$s">',
-        'after_widget' => '</section>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>'
-    ];
-    
-    register_sidebar([
-            'name' => __('Footer', 'sage'),
-            'id' => 'sidebar-footer'
-        ] + $config);
-});
-
-/**
  * Purge nginx cache on option update
  */
 add_action('updated_option', function() {
