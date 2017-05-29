96365bfc4900a51882b5524cb75d2b8ed80c7141
saraojelind <sara.ojelind@frojd.se>
2017-05-29 16:22:42 +0200

    Changed walker nav

--- src/app/themes/main/src/walkers/nav.php
+++ src/app/themes/main/src/walkers/nav.php
@@ -6,7 +6,8 @@ class Nav extends \Walker_Nav_Menu {
 
     public function start_lvl(&$output, $depth = 0, $args = array()) {
         $indent = str_repeat("\t", $depth);
-        $class = isset($args['menu_class']) ? $args['menu_class'] : 'menu';
+        $class = isset($args->menu_class) ? $args->menu_class : 'menu';
+        $class = isset($args->class) ? $args->class : $class;
         $output .= "\n$indent<ul class=\"" . $class . "__children\">\n";
     }
 
@@ -21,19 +22,21 @@ class Nav extends \Walker_Nav_Menu {
 
         $classes = array('item');
 
+        $children = false;
+
         if(is_array($item->classes)) {
-            $active = false;
             if(in_array('current_page_item', $item->classes) || in_array('current-menu-item', $item->classes)) {
                 $classes[] = 'item--current';
-                $active = true;
             }
             if(in_array('current-page-ancestor', $item->classes) || in_array('current-menu-ancestor', $item->classes)) {
                 $classes[] = 'item--ancestor';
-                $active = true;
             }
             if(in_array('current-page-parent', $item->classes) || in_array('current-menu-parent', $item->classes)) {
                 $classes[] = 'item--parent';
-                $active = true;
+            }
+            if(in_array('menu-item-has-children', $item->classes)) {
+                $classes[] = 'item--has-children';
+                $children = true;
             }
         }
 
@@ -51,17 +54,33 @@ class Nav extends \Walker_Nav_Menu {
         $output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="' . $classNames . '">';
 
         // link attributes
-        $attributes  = !empty($item->attr_title) ? ' title="'  . esc_attr($item->attr_title) . '"'  : '';
-        $attributes .= !empty($item->target)     ? ' target="' . esc_attr($item->target) . '"'      : '';
-        $attributes .= !empty($item->xfn)        ? ' rel="'    . esc_attr($item->xfn) . '"'         : '';
-        $attributes .= !empty($item->url)        ? ' href="'   . esc_attr($item->url) . '"'         : '';
-        $attributes .= ' class="' . $class . '__link"';
+        $attributes = ' class="' . $class . '__link' . ($children ? ' js-toggle-children' : '') . '"';
+        $tag = 'span';
 
-        $item_output = sprintf('%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+        if($children) {
+            $tag = 'a';
+        }
+
+        if(!empty($item->url)) {
+            $tag = 'a';
+            $attributes .= !empty($item->attr_title) ? ' title="'  . esc_attr($item->attr_title) . '"'  : '';
+            $attributes .= !empty($item->target)     ? ' target="' . esc_attr($item->target) . '"'      : '';
+            $attributes .= !empty($item->xfn)        ? ' rel="'    . esc_attr($item->xfn) . '"'         : '';
+            $attributes .= !empty($item->url)        ? ' href="'   . esc_attr($item->url) . '"'         : '';
+        }
+
+        $content = '<span class="' . $class . '__title">' . apply_filters('the_title', $item->title, $item->ID) . '</span>';
+
+        if($item->post_content && !empty($item->post_content)) {
+            $content .= '<span class="' . $class . '__content">' . nl2br($item->post_content) . '</span>';
+        }
+
+        $item_output = sprintf('%1$s<%2$s%3$s>%4$s%5$s%6$s</%2$s>%7$s',
             $args->before,
+            $tag
             $attributes,
             $args->link_before,
-            apply_filters('the_title', $item->title, $item->ID),
+            $content,
             $args->link_after,
             $args->after
         );
