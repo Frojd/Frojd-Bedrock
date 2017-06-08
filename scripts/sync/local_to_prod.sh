15cc1995f5074f5d2fddd5fe930d5270992a9a40
Martin Sandström <martin@marteinn.se>
2017-06-08 10:34:26 +0200

    Change naming convention from LIVE to PROD in syncscripts

--- scripts/sync/local_to_prod.sh
+++ scripts/sync/local_to_prod.sh
@@ -5,7 +5,7 @@
 # SSH-keys is mandatory
 # Example usage `scripts/sync/local_to_prod.sh`
 
-read -p "This will replace the LIVE database - Are you sure? [y/n]" -n 1 -r
+read -p "This will replace the PROD database - Are you sure? [y/n]" -n 1 -r
 echo # nl
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
@@ -18,14 +18,14 @@ source scripts/sync/STAGES
 
 docker-compose run web bash -c "cd /app; wp --allow-root db export db-dumps/latest.sql"
 
-scp docker/files/db-dumps/latest.sql $LIVE_USERNAME@$LIVE_HOSTNAME:/tmp/latest.sql
-ssh $LIVE_USERNAME@$LIVE_HOSTNAME "cd $LIVE_SRC_PATH;
+scp docker/files/db-dumps/latest.sql $PROD_USERNAME@$PROD_HOSTNAME:/tmp/latest.sql
+ssh $PROD_USERNAME@$PROD_HOSTNAME "cd $PROD_SRC_PATH;
     wp --allow-root db import /tmp/latest.sql;
-    wp --allow-root search-replace $LOCAL_DOMAIN $LIVE_DOMAIN;
+    wp --allow-root search-replace $LOCAL_DOMAIN $PROD_DOMAIN;
     wp --allow-root option set ep_host http://localhost:9200;
     wp --allow-root cache flush;
     wp --allow-root elasticpress index;"
 
-rsync -re ssh src/app/uploads/ $LIVE_USERNAME@$LIVE_HOSTNAME:$LIVE_UPLOAD_PATH
+rsync -re ssh src/app/uploads/ $PROD_USERNAME@$PROD_HOSTNAME:$PROD_UPLOAD_PATH
 
 cd -
