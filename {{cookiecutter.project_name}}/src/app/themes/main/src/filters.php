319757939d6d1373b79d48921987adb5e2f0f046
saraojelind <sara.ojelind@frojd.se>
2020-05-08 17:08:49 +0200

    Move some filters to setup instead

--- {{cookiecutter.project_name}}/src/app/themes/main/src/filters.php
+++ {{cookiecutter.project_name}}/src/app/themes/main/src/filters.php
@@ -2,65 +2,6 @@
 
 namespace App\Filters;
 
-use Roots\Sage\Template;
-use Roots\Sage\Template\Wrapper;
-
-/**
- * Determine which pages should NOT display the sidebar
- * @link https://codex.wordpress.org/Conditional_Tags
- */
-add_filter('sage/display_sidebar', function ($display) {
-    // The sidebar will NOT be displayed if ANY of the following return true
-    return $display ? !in_array(true, [
-        is_404(),
-        is_front_page(),
-        is_page_template('templates/template-custom.php'),
-    ]) : $display;
-});
-
-/**
- * Add <body> classes
- */
-add_filter('body_class', function (array $classes) {
-    // Add page slug if it doesn't exist
-    if (is_single() || is_page() && !is_front_page()) {
-        if (!in_array(basename(get_permalink()), $classes)) {
-            $classes[] = basename(get_permalink());
-        }
-    }
-
-    // Add class if sidebar is active
-    if (display_sidebar()) {
-        $classes[] = 'sidebar-primary';
-    }
-
-    return $classes;
-});
-
-/**
- * Add "… Continued" to the excerpt
- */
-add_filter('excerpt_more', function () {
-    return ' &hellip; <a href="' . get_permalink() . '">' . __('Read more', 'sage') . '</a>';
-});
-
-/**
- * Use theme wrapper
- */
-add_filter('template_include', function ($main) {
-    if (!is_string($main) && !(is_object($main) && method_exists($main, '__toString'))) {
-        return $main;
-    }
-    return ((new Template(new Wrapper($main)))->layout());
-}, 109);
-
-/**
- * Sanetize filenames on upload
- */
-add_filter('sanitize_file_name', function($filename) {
-    return preg_replace('/[^a-zA-Z0-9-_\.]/', '', $filename);
-}, 20);
-
 /**
  * Filter and remove WP image wrappers, replace with custom markup
  */
@@ -75,14 +16,3 @@ add_filter('the_content', function($content, $preg = true) {
     
    return $new;
 });
-
-add_filter('get_the_excerpt', function($str, $post = null) {
-    if(has_excerpt($post))
-        return $str;
-
-    $preamble = wp_strip_all_tags(get_field('preamble', $post->ID), true);
-    if(!empty($preamble))
-        return $preamble;
-
-    return $str;
-}, 10, 2);
--- {{cookiecutter.project_name}}/src/app/themes/main/src/setup.php
+++ {{cookiecutter.project_name}}/src/app/themes/main/src/setup.php
@@ -3,6 +3,7 @@
 namespace App\Setup;
 
 use Roots\Sage\Template;
+use Roots\Sage\Template\Wrapper;
 
 /**
  * Theme assets
@@ -14,6 +15,50 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, $verTag);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], $verTag, true);
 }, 100);
+
+/**
+ * Use theme wrapper
+ */
+add_filter('template_include', function ($main) {
+    if (!is_string($main) && !(is_object($main) && method_exists($main, '__toString'))) {
+        return $main;
+    }
+    return ((new Template(new Wrapper($main)))->layout());
+}, 109);
+
+/**
+ * Add <body> classes
+ */
+add_filter('body_class', function (array $classes) {
+    // Add page slug if it doesn't exist
+    if (is_single() || is_page() && !is_front_page()) {
+        if (!in_array(basename(get_permalink()), $classes)) {
+            $classes[] = basename(get_permalink());
+        }
+    }
+    return $classes;
+});
+
+/**
+ * Add preamble to fallback for excerpt
+ */
+add_filter('get_the_excerpt', function($str, $post = null) {
+    if(has_excerpt($post))
+        return $str;
+
+    $preamble = wp_strip_all_tags(get_field('preamble', $post->ID), true);
+    if(!empty($preamble))
+        return $preamble;
+
+    return $str;
+}, 10, 2);
+
+/**
+ * Sanetize filenames on upload
+ */
+add_filter('sanitize_file_name', function($filename) {
+    return preg_replace('/[^a-zA-Z0-9-_\.]/', '', $filename);
+}, 20);
 
 /**
  * Theme setup
