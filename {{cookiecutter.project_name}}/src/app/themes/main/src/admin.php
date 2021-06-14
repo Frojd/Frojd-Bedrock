e008485b696afceb830615122ec73e58c06c8fd6
saraojelind <sara.ojelind@frojd.se>
2021-06-14 12:42:05 +0200

    Add template name to pages list view and add default title to default template

--- {{cookiecutter.project_name}}/src/app/themes/main/src/admin.php
+++ {{cookiecutter.project_name}}/src/app/themes/main/src/admin.php
@@ -16,3 +16,53 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', \App\asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+/**
+ * Add name to default template
+ */
+add_filter('default_page_template_title', function() {
+    return __('Default template', 'sage');
+});
+
+/**
+ * Add template to page list view
+ */
+add_filter('manage_pages_columns', function($columns) {
+    return array_merge($columns, [
+        'template' => __('Template', 'sage')
+    ]);
+});
+add_filter('manage_edit-page_sortable_columns', function($columns) {
+    $columns['template'] = 'template';
+    return $columns;
+});
+add_action('manage_pages_custom_column', function($column, $postId) {
+    switch ($column) {
+        case 'template':
+            $templates = get_page_templates();
+            $template = get_page_template_slug($postId);
+            $name = array_search($template, $templates);
+            echo $name;
+            break;
+        default:
+            break;
+    }
+}, 10, 2);
+
+/**
+ * Add custom sorting to columns
+ */
+add_action('pre_get_posts', function($query) {
+    if (!is_admin())
+        return;
+
+    $orderby = $query->get('orderby');
+    switch ($orderby) {
+        case 'template':
+            $query->set('meta_key', '_wp_page_template');
+            $query->set('orderby', 'meta_value');
+            break;
+        default:
+            break;
+    }
+});
