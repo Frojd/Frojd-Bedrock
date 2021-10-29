ee78d048717c03d971ae4c7b171dabdd35637913
saraojelind <sara.ojelind@frojd.se>
2021-10-29 15:28:45 +0200

    Add two more favicons and move application settings to head

--- {{cookiecutter.project_name}}/src/app/themes/main/templates/partials/head.php
+++ {{cookiecutter.project_name}}/src/app/themes/main/templates/partials/head.php
@@ -3,6 +3,9 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="theme-color" content="#ffffff"/>
+    <meta name="application-name" content="<?= bloginfo('name'); ?>"/>
+    <meta name="msapplication-TileColor" content="#ffffff" />
+
     <?php get_template_part('partials/favicons'); ?>
 
     <?php if (SENTRY_DSN) : ?>
