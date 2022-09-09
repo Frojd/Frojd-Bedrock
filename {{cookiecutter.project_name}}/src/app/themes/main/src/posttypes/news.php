2d398e09292f5a1d98b1a719ac383fcb9fbd81ca
saraojelind <sara.ojelind@frojd.se>
2022-09-09 16:19:34 +0200

    Add news post type

---
+++ {{cookiecutter.project_name}}/src/app/themes/main/src/posttypes/news.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Posttypes\News;
+
+add_action('init', function() {
+    $single = _x('News', 'Post type single string', 'sage');
+    $plural = _x('News', 'Post type plural string', 'sage');
+    $labels = \App\Posttypes\get_labels($single, $plural);
+
+    $args = [
+        'label' => __('News', 'sage'),
+        'description' => '',
+        'labels' => $labels,
+        'supports' => [
+            'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author'
+        ],
+        'rewrite' => [
+            'slug' => 'news',
+        ],
+        'public' => true,
+        'show_in_rest' => true,
+        'has_archive' => true,
+        'hierarchical' => false,
+        'capability_type' => 'page',
+        'menu_icon' => 'dashicons-media-text',
+        'taxonomies' => ['category', 'post_tag'],
+        'menu_icon' => 'dashicons-testimonial',
+    ];
+    register_post_type('news', $args);
+}, 0);
