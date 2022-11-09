1bf8bd986d01f43f547df72269a335dd1a726f68
saraojelind <sara.ojelind@frojd.se>
2022-11-09 08:17:05 +0100

    Fix git flow setup file

--- {{cookiecutter.project_name}}/scripts/provision/setup_git_flow.sh
+++ {{cookiecutter.project_name}}/scripts/provision/setup_git_flow.sh
@@ -3,6 +3,10 @@
 git config gitflow.branch.master 'main'
 git flow init -d -t 'v'
 
-chmod 755 ./git-hooks/bump-version.sh
-ln -nfs ./git-hooks/bump-version.sh ./.git/hooks/post-flow-release-start
-ln -nfs ./git-hooks/bump-version.sh ./.git/hooks/post-flow-hotfix-start
+chmod +x $PWD/git-hooks/bump-version.sh
+ln -nfs $PWD/git-hooks/bump-version.sh .git/hooks/post-flow-release-start
+ln -nfs $PWD/git-hooks/bump-version.sh .git/hooks/post-flow-hotfix-start
+
+echo "Git flow and git hooks has been added"
+
+exit 0
