35ede2df59f27292fac78960aa6729c066bd9ad3
saraojelind <sara.ojelind@frojd.se>
2022-12-07 15:04:24 +0100

    Change header

--- {{cookiecutter.project_name}}/src/app/themes/main/templates/partials/header.php
+++ {{cookiecutter.project_name}}/src/app/themes/main/templates/partials/header.php
@@ -1,5 +1,20 @@
 <header class="header">
     <div class="header__container">
+        <div class="header__top">
+            <nav class="header__nav header__nav--service" aria-label="<?= __('Secondary navigation', 'sage'); ?>">
+                <?php
+                    if (has_nav_menu('service_navigation')) :
+                        wp_nav_menu([
+                            'theme_location' => 'service_navigation',
+                            'menu_class' => 'menu-service',
+                            'class' => 'menu-service',
+                            'container' => '',
+                            'walker' => new App\Walkers\Nav\Nav()
+                        ]);
+                    endif;
+                ?>
+            </nav>
+        </div>
         <div class="header__content">
             <a class="header__brand" href="<?= esc_url(home_url('/')); ?>">
                 <img
@@ -12,29 +27,16 @@
             <button type="button" class="header__toggle js-toggle-menu" data-toggle=".header__nav">
                 <span class="sr-only"><?= __('Toggle menu', 'sage'); ?></span>
             </button>
-            <div class="header__nav">
-                <nav class="header__menu" aria-label="<?= __('Primary navigation', 'sage'); ?>">
+            <div class="header__menu">
+                <nav class="header__nav header__nav--primary" aria-label="<?= __('Primary navigation', 'sage'); ?>">
                     <?php
                         if (has_nav_menu('primary_navigation')) :
                             wp_nav_menu([
                                 'theme_location' => 'primary_navigation',
-                                'menu_class' => 'menu menu--primary',
-                                'class' => 'menu',
+                                'menu_class' => 'menu-main',
+                                'class' => 'menu-main',
                                 'container' => '',
                                 'walker' => new App\Walkers\Nav\Nav(),
-                            ]);
-                        endif;
-                    ?>
-                </nav>
-                <nav class="header__service" aria-label="<?= __('Secondary navigation', 'sage'); ?>">
-                    <?php
-                        if (has_nav_menu('service_navigation')) :
-                            wp_nav_menu([
-                                'theme_location' => 'service_navigation',
-                                'menu_class' => 'menu menu--service',
-                                'class' => 'menu',
-                                'container' => '',
-                                'walker' => new App\Walkers\Nav\Nav()
                             ]);
                         endif;
                     ?>
